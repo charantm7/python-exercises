4da7afe08c6e114c573468deccfc7a20a11c85e7
CharanTM <charantm8787@gmail.com>
2024-04-19 23:32:34 +0530

    add function

--- sets.py
+++ sets.py
@@ -1,2 +1,9 @@
-set_collection = {"charan",13,5,3}
-print(set_collection)
+#set_collection = {"charan",13,5,3}
+#print(set_collection)
+set_collection1 = set()
+set_collection1.add(1)
+set_collection1.add(2)
+set_collection1.add(3)
+set_collection1.add(4)
+set_collection1.add(5)
+print(set_collection1)
