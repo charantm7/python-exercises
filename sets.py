b9ef80cd8be051c87f064deeff6f2ff81270b940
CharanTM <charantm8787@gmail.com>
2024-04-19 23:37:44 +0530

    pop in list

--- sets.py
+++ sets.py
@@ -1,11 +1,5 @@
 #set_collection = {"charan",13,5,3}
 #print(set_collection)
-set_collection1 = set()
-set_collection1.add(1)
-set_collection1.add(2)
-set_collection1.add(3)
-set_collection1.add(4)
-set_collection1.add(5)
-set_collection1.remove(5)
-set_collection1.pop()
-print(set_collection1)
+a = [1,2,3,4,5,6]
+a.pop(2)
+print(a)
