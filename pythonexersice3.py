dc0a7901310aface50da66e627ac085c6a91c4a2
CharanTM <charantm8787@gmail.com>
2024-04-23 23:39:44 +0530

    break statement

--- pythonexersice3.py
+++ pythonexersice3.py
@@ -1,10 +1,6 @@
-nums = (3,5,34,63,14,27,39,50)
-
-x = 5J
-idx = 0
-while idx < len(nums):
-    if(nums[idx]==x):
-        print("FOUND AT INDEX",idx)
-    else:
-        print("finding....")
-    idx += 1
+i = 1
+while i <= 5:
+    print(i)
+    if(i==3):
+        break
+    i += 1
