522500685ed56cf2c6d802097d620cd13a196e16
CharanTM <charantm8787@gmail.com>
2024-04-23 22:28:51 +0530

    new one

---
+++ pythonexersice3.py
@@ -0,0 +1,10 @@
+nums = (3,5,34,63,14,27,39,50)
+
+x = 5J
+idx = 0
+while idx < len(nums):
+    if(nums[idx]==x):
+        print("FOUND AT INDEX",idx)
+    else:
+        print("finding....")
+    idx += 1
