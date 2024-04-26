ba9e205f4b64071822249278f712bcf8db3c856e
CharanTM <charantm8787@gmail.com>
2024-04-26 16:24:32 +0530

    new code

--- pythonexersice3.py
+++ pythonexersice3.py
@@ -1,3 +1,7 @@
+MAX_LINES = 5
+MAX_BET = 200
+MIN_BET = 1
+
 def deposit():
     while True:
         amount = input("what would you like to deposit:$")
@@ -10,5 +14,41 @@ def deposit():
         else:
             print("please enter a number.")
     return amount
-deposit()
+
+
+def get_num_of_lines():
+    while True:
+        lines = input("enter the nmber of lines(1-"+ str(MAX_LINES) + ") ")        
+        if lines.isdigit():
+            lines = int(lines)
+            if 1 <= lines <= MAX_LINES:
+                break
+            else:
+                print("Enter the valid number of lines.")
+        else:
+            print("please enter a number.")
+    return lines
+
+def get_bet():
+    while True:
+       currency = input(f"Enter the amount of bet between ${MIN_BET} - ${MAX_BET}? ")
+       if currency.isdigit():
+            currency = int(currency)
+            if MIN_BET <= currency <= MAX_BET:
+                break
+            else:
+                print(f"Please entre the currency between ${MIN_BET} - ${MAX_BET}.")
+       else:
+            print("please enter a number.")
+    return currency
+
+
+def main():
+    balance = deposit()
+    lines = get_num_of_lines()
+    bet = get_bet()
+    total_bet = bet * lines
+    print(f"You are beting is {bet} on {lines} lines and your total bet is {total_bet}")
+    print(f"Your balance amount in your account is {balance}")
+main()
    
