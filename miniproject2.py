2b1ad90251e50aba65d67d7d1dffeb1a294ffd32
CharanTM <charantm8787@gmail.com>
2024-04-27 18:44:41 +0530

    updated

--- miniproject2.py
+++ miniproject2.py
@@ -15,7 +15,10 @@ else:
 
 random_number = random.randint(0,top_of_range)
 
+guesses = 0
+
 while True:
+    guesses += 1
     user_guess = input("Make a guess of a number: ")
     if user_guess.isdigit():
         user_guess = int(user_guess)
@@ -25,7 +28,11 @@ while True:
 
     if user_guess == random_number:
         print("Yeah you got it :) ")
-        quit()
+        break
     else:
-        print("You got it worng :(")
+        if user_guess > random_number:
+            print("The number is above from your guess!")
+        else:
+            print("The nuber is below from your guess!")
         
+print(f"You got in {guesses} guesses")
