d2e8d8b0072b10fbcbb1211745fcbf85e41a6b6c
CharanTM <charantm8787@gmail.com>
2024-04-27 18:17:11 +0530

    guess a number mini project

---
+++ miniproject2.py
@@ -0,0 +1,31 @@
+import random
+
+top_of_range = input("Enter the number: ")
+
+if top_of_range.isdigit():
+    top_of_range = int(top_of_range)
+
+    if top_of_range <= 0:
+        print("Please enter the number greater than 0 next time.")
+        quit()
+
+else:
+    print("Please enter the number.")
+    quit()
+
+random_number = random.randint(0,top_of_range)
+
+while True:
+    user_guess = input("Make a guess of a number: ")
+    if user_guess.isdigit():
+        user_guess = int(user_guess)
+    else:
+        print("Please enter the number next time.")
+        continue
+
+    if user_guess == random_number:
+        print("Yeah you got it :) ")
+        quit()
+    else:
+        print("You got it worng :(")
+        
