7d2d2e82c26af344e2c91d51927dd6490aceac0f
CharanTM <charantm8787@gmail.com>
2024-05-28 21:46:15 +0530

    new mini project

---
+++ miniproject4.py
@@ -0,0 +1,26 @@
+master_password = input("Please enter your password: ")
+
+def view():
+    pass
+
+def add():
+    name = input("Account name: ")
+    pwd = input("Password: ")
+
+    with open('password.txt','a') as f:
+        f.write(name + "|" + pwd + "\n")
+
+
+while True:
+
+    mode = input("Would you like to add a new password or to view the exciting ones (view,add) or press q to quit the mode: ").lower()
+
+    if mode == "q":
+        break
+    if mode == "view":
+        view()
+    elif mode == "add":
+        add()
+    else:
+        print("Invald input")
+        continue
