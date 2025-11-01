e9896d2312fd63d59649dcc890774db009b7c326
charantm <charantm8787@gmail.com>
2025-11-01 22:02:41 +0530

    .

--- Logics/loops_and_ranges/number_1_to_10.py
+++ Logics/loops_and_ranges/number_1_to_10.py
@@ -1,6 +1,6 @@
 # first version to print the number from 1 to 10
 print(*range(1, 11), sep=", ")
 
-# second version to print the numbers using for loop
+# second version to print the numbers using forloops
 for i in range(1, 11):
     print(i)
